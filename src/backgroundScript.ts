13dd3e68dbe32616cf3eaeccb7a99ddf3fd68814
Chris Fields II <chrisjr06@gmail.com>
2024-04-13 14:40:30 -0500

    opt out of yarn PnP so global types work

--- src/backgroundScript.ts
+++ src/backgroundScript.ts
@@ -1,3 +1,3 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts
 // Put all the javascript code here, that you want to execute in background.
-console.log('This is background script.');
+console.log('This is background script.')
