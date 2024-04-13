9e48215a2b882e22f5ca004387b6c50f02867995
Chris Fields II <chrisjr06@gmail.com>
2024-04-13 14:19:20 -0500

    add TS & prettier

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,2 +1,3 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
 // Put all the javascript code here, that you want to execute after page load.
+console.log('This is content script.');
