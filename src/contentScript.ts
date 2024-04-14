66d5667a9e98a56e218096b3788e798bf0bbb6d7
Chris Fields II <chrisjr06@gmail.com>
2024-04-14 10:37:28 -0500

    adjust tsconfig so can use import syntax

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,6 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
 
-console.log('importing browser')
-// import browser from 'webextension-polyfill'
-const browser = require('webextension-polyfill')
+import browser from 'webextension-polyfill'
 console.log('This is content script.', browser)
