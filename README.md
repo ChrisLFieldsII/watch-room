60bd914765e5fa7829dab4e2ee91e53870124e52
Chris Fields II <chrisjr06@gmail.com>
2024-04-14 11:00:47 -0500

    add jquery

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Debugging
 
 Follow this link to debug: https://extensionworkshop.com/documentation/develop/debugging/
+
+## Philosophies
+
+- The extension should listen to the video player events rather than re-inventing the wheel and having controls in the extension browserAction
--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,4 +1,9 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Content_scripts
 
 import browser from 'webextension-polyfill'
-console.log('This is content script.', browser)
+import $ from 'jquery'
+
+console.log('Content script loaded')
+
+const video = $('video')
+console.log('video', video)
