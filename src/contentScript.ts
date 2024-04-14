7e4912b8d4f90dc4c9f9fe069f66b8f58681f8a3
Chris Fields II <chrisjr06@gmail.com>
2024-04-14 17:01:48 -0500

    content script gets video element and attaches event handlers

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -5,5 +5,29 @@ import $ from 'jquery'
 
 console.log('Content script loaded')
 
-const video = $('video')
+const video = $<HTMLVideoElement>('video')
 console.log('video', video)
+
+function getVideoTime() {
+  return video.prop('currentTime')
+}
+
+function setVideoTime(time: number) {
+  video.prop('currentTime', time)
+}
+
+video.on('play', () => {
+  console.log('video play', getVideoTime())
+})
+
+video.on('pause', () => {
+  console.log('video pause', getVideoTime())
+})
+
+video.on('ended', () => {
+  console.log('video ended', getVideoTime())
+})
+
+video.on('seeked', () => {
+  console.log('video seeked', getVideoTime())
+})
