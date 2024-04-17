9581a0165ad00633131ca56b7ed71a4673917edb
Chris Fields II <chrisjr06@gmail.com>
2024-04-17 11:02:28 -0500

    setup websockets on client/server

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,8 +2,30 @@
 
 import browser from 'webextension-polyfill'
 import $ from 'jquery'
+import { io } from 'socket.io-client'
 
 console.log('Content script loaded')
+
+const socket = io('http://localhost:3000', { transports: ['websocket'] })
+
+socket.on('connect_error', (error) => {
+  console.log('socket connect_error', error.message)
+  if (socket.active) {
+    // temporary failure, the socket will automatically try to reconnect
+  } else {
+    // the connection was denied by the server
+    // in that case, `socket.connect()` must be manually called in order to reconnect
+    console.log(error.message)
+  }
+})
+socket.on('connect', () => {
+  console.log(socket.id)
+
+  socket.emit('events', { test: 'test' })
+})
+socket.on('events', (data) => {
+  console.log('events', data)
+})
 
 const video = $<HTMLVideoElement>('video')
 console.log('video', video)
