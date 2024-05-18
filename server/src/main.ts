6005a6a75098956e358e105e4b084f3b2e1b70c4
Chris Fields II <chrisjr06@gmail.com>
2024-05-18 17:00:14 -0500

    nest copies secrets dir to dist, fix reading tls certs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: {
-      // key: readFileSync('./src/secrets/local-key.pem'),
-      // cert: readFileSync('./src/secrets/local-cert.pem'),
-      key: readFileSync('/Users/cri/.local-ca/localhost+2-key.pem'),
-      cert: readFileSync('/Users/cri/.local-ca/localhost+2.pem'),
+      key: readFileSync(resolve(__dirname, 'secrets/localaws-key.pem')),
+      cert: readFileSync(resolve(__dirname, 'secrets/localaws-cert.pem')),
     },
   });
   await app.listen(3000);
