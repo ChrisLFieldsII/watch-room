750d999223797bb4a9f9925145ce7797fad3c52a
Chris Fields II <chrisjr06@gmail.com>
2024-05-23 21:07:59 -0500

    https is live on ec2 instance!

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { ShutdownObserver } from './shutdownObserver';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    EventsModule,
+    ConfigModule.forRoot({
+      envFilePath: process.env.ENV === 'dev' ? '.env.dev' : '.env.prod',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ShutdownObserver],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { readFileSync } from 'fs';
-import { resolve } from 'path';
-import * as http from 'node:http';
-import { ShutdownObserver } from './shutdownObserver';
 
 async function bootstrap() {
+  console.log(
+    `ENV=${process.env.ENV}\n`,
+    `HTTPS_KEY_PATH=${process.env.HTTPS_KEY_PATH}\n`,
+    `HTTPS_CERT_PATH=${process.env.HTTPS_CERT_PATH}`,
+  );
+
   const app = await NestFactory.create(AppModule, {
     httpsOptions: {
-      key: readFileSync(resolve(__dirname, 'secrets/localaws-key.pem')),
-      cert: readFileSync(resolve(__dirname, 'secrets/localaws-cert.pem')),
+      key: readFileSync(process.env.HTTPS_KEY_PATH!),
+      cert: readFileSync(process.env.HTTPS_CERT_PATH!),
     },
   });
   await app.listen(3000);
-
-  // need a separate http server for certbot: https://certbot.eff.org/instructions?ws=other&os=arch
-  const httpServer = http.createServer(app.getHttpAdapter().getInstance());
-  httpServer.listen(8080);
-
-  const shutdownObserver = app.get(ShutdownObserver);
-  shutdownObserver.addHttpServer(httpServer);
 }
 bootstrap();
