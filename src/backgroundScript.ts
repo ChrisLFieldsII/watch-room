8c924c521bc67eb6122257b9c65f6bc4d9e1a71e
Chris Fields II <chrisjr06@gmail.com>
2024-06-20 22:58:30 -0500

    rm unused backgroundScript file

--- src/backgroundScript.ts
+++
@@ -1,5 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Background_scripts
-
-import browser from 'webextension-polyfill'
-
-console.debug('This is background script.')
